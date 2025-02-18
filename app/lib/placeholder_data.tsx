1835109f688579e7037bb24070266ab4b8c9a093
Charlie <charlienino@gmail.com>
2025-02-18 09:06:02 -0800

    Add schema

---
+++ app/lib/placeholder_data.tsx
@@ -0,0 +1,22 @@
+const sample_mtrx = [
+    {
+        date: "11-01-24",
+        theme: "On the street",
+        rows: [
+                {
+                    clue:  {
+                        "S": ["Spinning Toy.", "Peak."]
+                    },
+                    solution: "top",
+                },
+                {
+                    clue:  {
+                        "": ["Legal action.", "Button on a calculator."]
+                    },
+                    solution: "sign",
+                },
+            ]
+    }
+]
+
+export { sample_mtrx };
